ba8dddbff940102b06c28ae71ba15dd98c97f699
naqi10 <naqizaidi2112002@gmail.com>
2025-06-16 16:22:10 +0500

    Fix: correct LatestNews import path

--- src/components/Latestnews.jsx
+++ src/components/Latestnews.jsx
@@ -1,5 +1,5 @@
 
-export default function LatestNews() {
+export default function Latestnews() {
     return (
         <section id="blog" className="py-16 bg-white dark:bg-gray-900">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
